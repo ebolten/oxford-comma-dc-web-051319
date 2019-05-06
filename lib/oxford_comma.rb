f686be18ab56361110c801c7e6ae92573a54dd29
Emily Bolten <emilybolten@gmail.com>
2019-05-06 17:01:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   stringNames = ""
   for i in 0..array.length - 1
-    stringNames += array[i].join(", ")
+    stringNames << array[i].join(", ")
   end
   return stringNames
 end
