7fb09cc2337ab8588997cfd26f658f0227c226aa
Emily Bolten <emilybolten@gmail.com>
2019-05-06 17:22:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,16 @@
 def oxford_comma(array)
 
   if array.length < 2
-    stringNames = array[0]
-    return stringNames
+    return array[0]
+
   elsif array.length == 2
-    stringNames = array.join(" and ")
-    return stringNames
+    return array.join(" and ")
+
   elsif array.length >= 3
     andItem = "and #{array[-1]}"
     array.pop
     array.push(andItem)
-    stringArray = array.join(", ")
-    return stringNames
+    return array.join(", ")
+
   end
 end
