819fb3ba28b2ad7238b8d24cbd4dafe56649b2d0
Emily Bolten <emilybolten@gmail.com>
2019-05-06 17:07:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   if array.length == 2
     stringNames = array.split(" and ")
   elsif array.length == 2
-    stringNames = array[0:1].split(", ")
+    stringNames = array[1:0].split(", ")
     stringNames << array[2].split(" and ")
   end
 
