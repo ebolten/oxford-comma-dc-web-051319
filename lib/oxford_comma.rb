a9fd5e6958f05798686597e18be21b02ef9f5104
Emily Bolten <emilybolten@gmail.com>
2019-05-06 17:22:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  if array.length < 2
+    return array[0]
+
+  elsif array.length == 2
+    return array.join(" and ")
+
+  elsif array.length >= 3
+    andItem = "and #{array[-1]}"
+    array.pop
+    array.push(andItem)
+    return array.join(", ")
+
+  end
 end
